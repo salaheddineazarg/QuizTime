d33d17dac030c4af75c79ea6c82b5368503d14e6
salaheddineazarg <salahazarag17@gmail.com>
2023-10-30 10:11:12 +0100

    Structeur

---
+++ src/test/java/com/spring/quiztime/QuizTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.quiztime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
