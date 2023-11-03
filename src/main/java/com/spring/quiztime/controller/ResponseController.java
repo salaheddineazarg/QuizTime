28ff4aeee67737fa798b27a1fae5999db1d4682b
salaheddineazarg <salahazarag17@gmail.com>
2023-11-03 16:35:35 +0100

    responseFunction

---
+++ src/main/java/com/spring/quiztime/controller/ResponseController.java
@@ -0,0 +1,55 @@
+package com.spring.quiztime.controller;
+
+
+import com.spring.quiztime.entities.Level;
+import com.spring.quiztime.entities.Response;
+import com.spring.quiztime.service.ResponseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/response")
+public class ResponseController {
+
+    @Autowired
+    private ResponseService responseService;
+
+
+
+    @GetMapping
+    public  List<Response> getAll(){
+
+        return responseService.getAllService();
+    }
+
+    @PostMapping
+    public ResponseEntity<Response> save(@RequestBody Response response){
+
+
+        return ResponseEntity.ok(responseService.saveService(response));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Response> update(@RequestBody Response response,@PathVariable("id") Long id){
+
+        return ResponseEntity.ok(responseService.updateService(response,id));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Long id){
+
+        responseService.deleteService(id);
+
+        return ResponseEntity.ok("Response deleted successfully!.");
+    }
+
+    @GetMapping("/getLevel/{id}")
+    public ResponseEntity<Optional<Response>> getById(@PathVariable("id") Long id) {
+
+        return ResponseEntity.ok(responseService.findByIdService(id));
+    }
+}
