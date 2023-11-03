628dd70415381c189312f9df832a6d09b8f7ce1d
salaheddineazarg <salahazarag17@gmail.com>
2023-11-03 17:09:32 +0100

    QT-2

--- src/main/java/com/spring/quiztime/controller/ResponseController.java
+++ src/main/java/com/spring/quiztime/controller/ResponseController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/response")
+@RequestMapping("/api/response")
 public class ResponseController {
 
     @Autowired
