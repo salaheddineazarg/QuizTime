4cb997d8de9f9ccf245ebf08b6a8924368081a84
salaheddineazarg <salahazarag17@gmail.com>
2023-11-10 13:31:31 +0100

    QT-1

--- src/main/java/com/spring/quiztime/service/QuestionService.java
+++ src/main/java/com/spring/quiztime/service/QuestionService.java
@@ -3,6 +3,7 @@ package com.spring.quiztime.service;
 
 import com.spring.quiztime.entities.Question;
 import com.spring.quiztime.repository.QuestionRepository;
+import com.spring.quiztime.service.interfaces.IQuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,18 +11,20 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class QuestionService {
+public class QuestionService implements IQuestionService {
 
     @Autowired
    private QuestionRepository questionRepository;
 
 
 
+    @Override
     public List<Question> getAllService(){
 
         return questionRepository.findAll();
     }
 
+    @Override
     public Optional<Question> saveService(Question question){
         int numberAnswers = question.getNumberAnswers();
         int trueAnswers =question.getNumberCorrectAnswers();
@@ -33,6 +36,7 @@ public class QuestionService {
         return Optional.empty();
     }
 
+    @Override
     public boolean deleteService(Long id){
         if(questionRepository.findById(id).isPresent()){
             questionRepository.deleteById(id);
@@ -40,7 +44,7 @@ public class QuestionService {
         }
         return false;
     }
-
+    @Override
     public Optional<Question> updateService(Question question,Long id){
         if(questionRepository.findById(id).isPresent()){
             question.setId(id);
@@ -48,7 +52,7 @@ public class QuestionService {
         }
         return null;
     }
-
+    @Override
     public Optional<Question> findByIdService(Long id){
 
         return questionRepository.findById(id);
