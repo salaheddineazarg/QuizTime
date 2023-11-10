5ab542b93585b849e8d09d8b69ee04db908a3cbd
salaheddineazarg <salahazarag17@gmail.com>
2023-11-10 13:42:36 +0100

    QT-3

--- src/main/java/com/spring/quiztime/service/LevelService.java
+++ src/main/java/com/spring/quiztime/service/LevelService.java
@@ -3,6 +3,7 @@ package com.spring.quiztime.service;
 
 import com.spring.quiztime.entities.Level;
 import com.spring.quiztime.repository.LevelRepository;
+import com.spring.quiztime.service.interfaces.ILevelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,23 +11,23 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class LevelService {
+public class LevelService implements ILevelService {
 
 
     @Autowired
     private LevelRepository levelRepository;
 
-
+    @Override
     public List<Level> getAllService(){
 
         return levelRepository.findAll();
     }
-
+    @Override
     public Optional<Level> saveService(Level level){
 
       return Optional.of(levelRepository.save(level));
     }
-
+    @Override
     public boolean deleteService(Long id){
 
         if(levelRepository.findById(id).isPresent()){
@@ -35,7 +36,7 @@ public class LevelService {
         }
         return false;
     }
-
+    @Override
     public Optional<Level> updateService(Level level,Long id){
         if(levelRepository.findById(id).isPresent()){
             level.setId(id);
@@ -43,7 +44,7 @@ public class LevelService {
         }
         return null;
     }
-
+    @Override
     public Optional<Level> findByIdService(Long id){
 
     return levelRepository.findById(id);
