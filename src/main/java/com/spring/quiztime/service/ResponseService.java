3ae765697b90be9ebd1baf2ffe5186b5a0e94244
salaheddineazarg <salahazarag17@gmail.com>
2023-11-10 13:46:42 +0100

    QT-2

--- src/main/java/com/spring/quiztime/service/ResponseService.java
+++ src/main/java/com/spring/quiztime/service/ResponseService.java
@@ -3,6 +3,7 @@ package com.spring.quiztime.service;
 
 import com.spring.quiztime.entities.Response;
 import com.spring.quiztime.repository.ResponseRepository;
+import com.spring.quiztime.service.interfaces.IResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,40 +11,44 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ResponseService {
+public class ResponseService implements IResponseService {
 
     @Autowired
     private ResponseRepository responseRepository;
 
 
-
-    public List<Response> getAllService(){
+    @Override
+    public List<Response> getAllService() {
 
         return responseRepository.findAll();
     }
 
-    public Optional<Response> saveService(Response response){
+    @Override
+    public Optional<Response> saveService(Response response) {
 
         return Optional.of(responseRepository.save(response));
     }
 
-    public boolean deleteService(Long id){
-        if(responseRepository.findById(id).isPresent()){
-        responseRepository.deleteById(id);
-        return true;
+    @Override
+    public boolean deleteService(Long id) {
+        if (responseRepository.findById(id).isPresent()) {
+            responseRepository.deleteById(id);
+            return true;
         }
         return false;
     }
 
-    public Optional<Response> updateService(Response response,Long id){
-        if(responseRepository.findById(id).isPresent()){
+    @Override
+    public Optional<Response> updateService(Response response, Long id) {
+        if (responseRepository.findById(id).isPresent()) {
             response.setId(id);
             return Optional.of(responseRepository.save(response));
         }
         return null;
     }
 
-    public Optional<Response> findByIdService(Long id){
+    @Override
+    public Optional<Response> findByIdService(Long id) {
 
         return responseRepository.findById(id);
     }
