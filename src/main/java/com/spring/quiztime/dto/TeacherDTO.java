ef2c4a1537a34989af9665d2ba7a1f1d3036bc04
salaheddineazarg <salahazarag17@gmail.com>
2023-11-17 19:25:16 +0100

    QT-6<Teacher> QT-20.QT-21.QT-52.QT.53.QT.54

--- src/main/java/com/spring/quiztime/dto/TeacherDTO.java
+++ src/main/java/com/spring/quiztime/dto/TeacherDTO.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Data
 @NoArgsConstructor
